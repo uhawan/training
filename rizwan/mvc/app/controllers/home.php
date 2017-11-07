e4abad73e20e0bb87bee8d692c8b8e815bf76d64
Rizwan <mrizwan.shahid32@gmail.com>
2017-11-07 17:09:38 +0500

    commit mvc

--- rizwan/mvc/app/controllers/home.php
+++ rizwan/mvc/app/controllers/home.php
@@ -1,9 +1,60 @@
 <?php
-class Home extends Controller
+class home extends Controller
 {
-    public function index()
+
+    protected $user;
+    public function __construct()
     {
-        echo 'home/index';
+        $this->User=$this->model('User');
     }
-}
+
+    public function index($name='')
+    {
+      $user= $this->user;
+
+      //$user->name=$name;
+         // echo $user->name
+        // echo $name.' '.$othername;
+
+        $this->view('home/index',['name'=> $user->name]);
+
+        /*user::create(
+            [
+                'username'=>'alex',
+                'email'=>'alex@ii.com'
+
+            ]);
+    */
+
+    }
+    /*
+
+    Public function create($username='',$email='')
+    {
+        User::create([
+           'username'=>$username,
+            'email'=>$email
+        ]);
+
+
+    }
+    */
+    Public function add($username='',$email='')
+    {
+        if (isset($_POST["add"])) {
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            User::create([
+                'username'=>$username,
+                'email'=>$email
+            ]);
+            $this->view('home/view');
+        }else{
+            $this->view('home/add');
+        }
+
+
+    }
+    
+    }
 ?>
--- rizwan/mvc/app/core/Controller.php
+++ rizwan/mvc/app/core/Controller.php
@@ -1,6 +1,17 @@
 <?php
 class Controller
 {
+    public function model($model)
+    {
+        require_once '../app/models/'.$model.'.php';
+        return new $model();
+        //echo $model;
+    }
+    public function view($view,$data=[])
+    {
+
+        require_once '../app/views/'.$view.'.php';
+}
 
 }
 
